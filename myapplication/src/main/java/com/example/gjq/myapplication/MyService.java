2b4b05f63fb2097307cdfeec5416efa55d67bcb4
guojiqing <369608703@qq.com>
2017-03-24 17:08:15 +0800

    第四次修改

--- myapplication/src/main/java/com/example/gjq/myapplication/MyService.java
+++ myapplication/src/main/java/com/example/gjq/myapplication/MyService.java
@@ -9,8 +9,8 @@ public class MyService extends Service {
     }
 
     @Override
-    public void onStart(Intent intent, int startId) {
-        super.onStart(intent, startId);
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
